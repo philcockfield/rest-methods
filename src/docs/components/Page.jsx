6ea4a357782110596c07df1ec9a9c157f7a1de26
Phil Cockfield <phil@cockfield.net>
2015-07-18 13:52:51 +1200

    Basic shell styling for docs

--- src/docs/components/Page.jsx
+++ src/docs/components/Page.jsx
@@ -12,7 +12,7 @@ export default class Page extends React.Component {
           <title>{ this.props.pageTitle }</title>
           <meta charSet='utf-8'/>
           <link href={ `${ this.props.basePath }/style.css` } rel='stylesheet'/>
-          <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'/>
+          <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,700,300italic,400italic' rel='stylesheet' type='text/css'/>
         </head>
         <body>
           { this.props.children }
