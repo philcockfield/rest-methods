a13be609dd54941cfef86f77eda4aae87e6f5d26
Phil Cockfield <phil@cockfield.net>
2015-07-20 10:20:52 +1200

    Style tweaks.

--- example/server-methods.js
+++ example/server-methods.js
@@ -1,6 +1,5 @@
 import Server from '../server';
 
-
 var server = Server({
   name:'My Service',
   version: '1.0.1',
@@ -10,21 +9,13 @@ var server = Server({
 
 
 server.methods({
-  'foo': function(id, text) {
-      console.log('invoking foo', id);
-      // throw new Error('ouch')
-      return {
-        id: id,
-        text: text,
-        date: new Date()
-      }
-  },
-
   'bar': {
     url: '/user/:id',
     docs:
       `Foo
-       Lorem \`ipsum\` dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+       Lorem \`ipsum\` dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 
        - Foo
        - bar
@@ -33,7 +24,8 @@ server.methods({
        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 
 
-       @param {string} id: The ID Lorem ipsum dolor sit amet, consectetur \`adipisicing\` elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+       @param {string} id: The ID Lorem ipsum dolor sit amet, consectetur \`adipisicing\` elit, sed do eiusmod tempor incididunt ut
+                           labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
        @return {object}
       `,
 
@@ -41,9 +33,21 @@ server.methods({
   },
 
 
+  'foo': function(id, text) {
+      // console.log('invoking foo', id);
+      // throw new Error('ouch')
+      return {
+        // id: id,
+        text: text,
+        date: new Date()
+      }
+  },
+
+
+
   'baz': {
     put: () => {}
-  }
+  },
 
 
 });
